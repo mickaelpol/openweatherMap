69f9c03caa1570b40151ce32b7198bd37187775e
Mickael <polmickael3@gmail.comgit config --global user.name>
2017-09-05 17:03:13 +0200

    push avec début jquery

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 $.ajax({
-	url:"http://history.openweathermap.org/data/2.5/history/city?id={3028640}&type=hour&start={start}&end={end}&APPID={a46b3c46d72e846fc8c834a487a2b41d}",
+
+	url:"http://api.openweatherma.org/data/2.5/forecast?id={6447020}&APPID=a46b3c46d72e846fc8c834a487a2b41d",
 	
-	// id: "3028640",
-	// name: "Arrondissement de Carcassonne",
-	// country: "FR",
-	// coord: {
-	// 	lon: 2.16667,
-	// 	lat: 43.25
-	// }
+	success:function(data){
+
+	},
+	erreur:function(data){
+		$('#jour1').html("Erreur!");
+		$('#jour2').html("Erreur!");
+		$('#jour3').html("Erreur!");
+	}
 });
